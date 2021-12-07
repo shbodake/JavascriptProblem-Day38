ffb472a55e8bc45bdd806ff94784fdae0f60b1b9
shbodake <sayalibodake28@gmail.com>
2021-12-07 12:11:15 +0530

    Added: Program of selection with case

---
+++ RepetationForLoop/NthHarmonic.js
@@ -0,0 +1,8 @@
+var prompt = require('prompt-sync')();
+var number = prompt("Enter the number : ");
+
+var hormonic = 0;
+for(let i = 1; i <= number; i++) {
+    hormonic = 1/i + hormonic;
+    console.log("Harmonic number of " + i + " is : " + hormonic);
+}
