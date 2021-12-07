582f60cb3be4009f4fb5da1a1da2c75273845753
shbodake <sayalibodake28@gmail.com>
2021-12-07 12:10:10 +0530

    Added: Program of repetation for loop

---
+++ RepetationForLoop/PowerOf2.js
@@ -0,0 +1,13 @@
+const prompt = require('prompt-sync')({ sigint: true });
+
+const n1 = Number(prompt("Enter a number: "));
+
+if (n1 >= 0 && Number.isInteger(n1)) {
+    for (let i = 0; i <= n1; i++) {
+        console.log("2^" + i + " = " + Math.pow(2, i));
+    }
+} 
+else
+{
+console.log("Enter only non-negative integers.");
+}
